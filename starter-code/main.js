3a5c21281988e4ca516bab76a97f650e49ad97b0
Default User <User1@User1s-MacBook-Pro.local>
2016-06-07 18:20:38 +0100

    Start of prework assignment 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,34 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+
+// if (cardTwo === cardFour) {
+// 	alert("You found a match!");
+// }
+// else {
+// 	alert("Sorry, try again.");
+// }
+
+document.getElementsByClassName('board')[0].setAttribute('id', 'game-board');
+
+var gameBoard = document.getElementById('game-board');
+
+for (var i = 0; i < 4; i++) {
+	var card = document.createElement('div');
+	card.className = 'card';
+	gameBoard.appendChild(card);
 }
-else {
-	alert("Sorry, try again.");
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var card = document.createElement('div');
+	card.className = 'card';
+	card.setAttribute('data-card', cards[i]);
+	gameBoard.appendChild(card);
+
+	}
+
 }
