eed55f6a69d95f32bbe3d9a34188e081c8aa9ceb
Default User <User1@User1s-MacBook-Pro.local>
2016-06-09 14:25:16 +0100

    Final version of assignment 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,53 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
+document.getElementsByClassName('board')[0].setAttribute('id', 'game-board');
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var card = document.createElement('div');
+	card.className = 'card';
+	card.setAttribute('data-card', cards[i]);
+	card.setAttribute('data-cardId', i)
+	card.addEventListener('click', isTwoCards);
+	gameBoard.appendChild(card);
+
+	}
+}
+var isMatch = function(inPlay) {
+	if (inPlay[0][1] == inPlay[1][1]) {
+		alert('Well done the cards match!');
+	}
+	else {
+		alert('You failed!')
+	}
+}
+var isTwoCards = function() {
+	var cardType = this.getAttribute('data-card');
+	var cardId = this.getAttribute('data-cardId');
+	if (cardsInPlay == 0 || cardId != cardsInPlay[0][0]) {
+		cardsInPlay.push([cardId, cardType]);
+		if (cardType == 'king') {
+			// this.innerHTML = '<img src="Card_Images/Spades 13.png" alt="King of spades"/>';
+			this.style.backgroundImage = "url('Card_Images/Spades 13.png')"
+		}
+		else {
+			// this.innerHTML = '<img src="Card_Images/Spades 12.png" alt="Queen of spades"/>'
+			this.style.backgroundImage = "url('Card_Images/Spades 12.png')"
+		}
+		if (cardsInPlay.length == 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+			var cardReset = document.getElementsByClassName('card');
+			for (var i = 0; i < cardReset.length; i++) {
+				// cardReset[i].innerHTML = "";
+				cardReset[i].style.backgroundImage = "url('Card_Images/_Back.png')"
+			}
+		}
+	}
+}
+
+createBoard();
 
 
 // if (cardTwo === cardFour) {
@@ -14,23 +57,13 @@ var cardsInPlay = [];
 // 	alert("Sorry, try again.");
 // }
 
-document.getElementsByClassName('board')[0].setAttribute('id', 'game-board');
+// var cardOne = 'queen';
+// var cardTwo = 'queen';
+// var cardThree = 'king';
+// var cardFour = 'king';
 
-var gameBoard = document.getElementById('game-board');
-
-for (var i = 0; i < 4; i++) {
-	var card = document.createElement('div');
-	card.className = 'card';
-	gameBoard.appendChild(card);
-}
-
-var createBoard = function() {
-	for (var i = 0; i < cards.length; i++) {
-	var card = document.createElement('div');
-	card.className = 'card';
-	card.setAttribute('data-card', cards[i]);
-	gameBoard.appendChild(card);
-
-	}
-
-}
+// for (var i = 0; i < 4; i++) {
+// 	var card = document.createElement('div');
+// 	card.className = 'card';
+// 	gameBoard.appendChild(card);
+// }
